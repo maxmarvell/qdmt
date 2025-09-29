cb381e5fbea660e778663c44546b66b27602fd0c
maxmarvell <max.marvell@hotmail.co.uk>
2025-09-29 11:21:07 +0100

    temporary new utils folder

---
+++ src/qdmt/utils_n/fd.py
@@ -0,0 +1,8 @@
+from qdmt.transfer_matrix import AbstractTransferMatrix
+
+# apply power function and derivative to Transfer Matrix class
+
+def power(E: AbstractTransferMatrix):
+    tape = []
+    pass
+
