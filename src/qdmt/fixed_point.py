9422987badec0e447e3824352f9758ae9323aa44
maxmarvell <max.marvell@hotmail.co.uk>
2025-09-29 11:25:27 +0100

    remove left fixed point class, irrelevent for our interest.

--- src/qdmt/fixed_point.py
+++ src/qdmt/fixed_point.py
@@ -24,7 +24,7 @@ def EtildeLeft(E: TransferMatrix, r, v):
 class RightFixedPoint():
 
     E: TransferMatrix
-    tensor: np.ndarray
+    array: np.ndarray
 
     def __init__(self, E: TransferMatrix):
         self.E = E
@@ -35,7 +35,7 @@ class RightFixedPoint():
         r *= np.sign(np.trace(r))
 
         ## assumes left canonical
-        self.tensor = r / np.trace(r)
+        self.array = r / np.trace(r)
 
     @classmethod
     def from_mps(cls, u_mps: UniformMps):
@@ -43,48 +43,25 @@ class RightFixedPoint():
         return cls(E)
 
     def derivative(self, v) -> np.ndarray:
-        _, nb = self.tensor.shape
+        _, nb = self.array.shape
         L = LinearOperator((nb ** 2, nb ** 2), matvec=partial(self._pseudoinverse_operator))
         Rh = gmres(L, v.reshape(nb ** 2))[0]
         Rh = Rh.reshape(nb, nb)
-        return ncon((Rh, self.E.B.tensor, self.tensor), ((-1, 1), (1, -2, 2), (2, -3)))
+        return ncon((Rh, self.E.B.array, self.array), ((-1, 1), (1, -2, 2), (2, -3)))
     
     def _pseudoinverse_operator(self: Self, v: np.ndarray):
         
-        d, _ = self.tensor.shape
+        d, _ = self.array.shape
         v = v.reshape(d, d)
 
         # transfermatrix contribution
-        transfer = ncon((v, self.E.tensor), ((1, 2), (2, 1, -2, -1)))
+        transfer = ncon((v, self.E.array), ((1, 2), (2, 1, -2, -1)))
 
         # fixed point contribution
-        fixed = np.trace(v @ self.tensor) * np.eye(d)
+        fixed = np.trace(v @ self.array) * np.eye(d)
 
         # sum these with the contribution of the identity
         return v - transfer + fixed
-
-class LeftFixedPoint():
-
-    def __init__(self, E: TransferMatrix):
-        self.shape = (E.d1, E.d2)
-        self.E = E
-
-        _, l = eigs(E.to_matrix().T, k=1, which='LM')
-        l = l.reshape((E.d1, E.d2))
-        l /= (np.trace(l) / np.abs(np.trace(l)))
-        l = (l + l.conj().T) / 2
-        l *= np.sign(np.trace(l))
-
-        ## assume left canonical form so should canonicalize to 
-        self.tensor = l / lA.tensor[0, 0]
-
-    def derivative(self, v) -> np.ndarray:
-        _, nb = self.shape
-        L = LinearOperator((nb ** 2, nb ** 2), matvec=partial(EtildeLeft, self.E, self.tensor))
-        Rh = gmres(L, v.reshape(nb ** 2))[0]
-        Rh = Rh.reshape(nb, nb)
-        return ncon((Rh, self.E.B.tensor, self.tensor), ((-1, 1), (1, -2, 2), (2, -3)))
-
         
 if __name__ == "__main__":
     pass
